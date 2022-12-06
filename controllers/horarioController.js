00bdc63f1a70f60fb9738d48b3db7fa780ed82ef
emanuel <emanueljoao80@gmail.com>
2022-12-06 10:59:34 +0000

    horario-alt

--- controllers/horarioController.js
+++ controllers/horarioController.js
@@ -6,66 +6,68 @@ const ObjectId = require('mongodb').ObjectId;
 
 
 exports.insert = (req, res) => {
-    CarreiraModel.carreiraFindOne(req.Carreira,(cdocs,err) =>{
-        if (err) res.status(500).send({message: err.message});     
-        FrequenciaModel.frequenciaFindOne(req.Frequencia,(fdocs,err) =>{
-            if (err) res.status(500).send({message: err.message});                        
-            HorarioModel.findHorarioCarreriaID (req.body.Carreira, (fhdoc,err) => {
-                //console.log("---------\n"+fhdoc[0]._id+"\n-------\n");
-                //console.log("teste\n"+req.body.Carreira);
-                console.log("teste\n"+fhdoc[0].lenght);
-                if(fhdoc.lenght)
-                HorarioModel.findHorarioFrequenciaID (req.body.Frequencia, (fhfdoc,err) => {
-                    if(fhfdoc){                                     
-                
-                        res.redirect("/horario");
-                    }else{
-                HorarioModel.updateHorarioFrequencia(fhdoc[0]._id,req.body, (doc,err) => {                
-                    if(!err) res.redirect("/horario");
-                    else res.redirect("/horario/create");
-                });
-            }
-            
-        });
-        else{
-            HorarioModel.createHorario(req.body, (doc,err) => {
-                if(!err) res.redirect("/horario");
-                else res.redirect("/horario/create");
+    // CarreiraModel.carreiraFindOne(req.Carreira,(cdocs,err) =>{
+    //     if (err) res.status(500).send({message: err.message});     
+    //     FrequenciaModel.frequenciaFindOne(req.Frequencia,(fdocs,err) =>{
+    //         if (err) res.status(500).send({message: err.message});                        
+    HorarioModel.findHorarioCarreriaID(req.body.Carreira, (hcdoc, err) => {
+        console.log("---------\n" + err + "\n-------\n");
+        console.log("---------\n" + hcdoc.Carreira + "\n-------\n");
+        console.log("********\n" + req.body.Carreira + "\n********\n");
+        console.log("teste\n" + hcdoc);
+        if (hcdoc?.lenght ?? false) {
+            HorarioModel.findHorarioFrequenciaID(req.body.Frequencia, (hfdoc, err) => {
+                if (hfdoc ?? false) {
+
+                    res.redirect("/horario");
+                } else {
+                    HorarioModel.updateHorarioFrequencia(hcdoc[0]._id, req.body, (hdoc, err) => {
+                        if (!err) { res.redirect("/horario"); }
+                        else { res.redirect("/horario/create"); }
+                    });
+                }
+
+            })
+        }
+        else {
+            HorarioModel.createHorario(req.body, (doc, err) => {
+                if (!err) { res.redirect("/horario"); }
+                else { res.redirect("/horario/create"); }
             });
 
         };
     });
-});
-});
+    //});
+    //});
 };
-                
-              
-                    
-                
-                           
+
+
+
+
+
 
 exports.listHorario = (req, res) => {
     HorarioModel.horarioList((docs, err) => {
         const isAuthenticated = !!req.user;
-        const num = 0; 
-        
-        if(!err) res.status(200).render('horario',{isAuthenticated,horario:docs,num});
-        else res.status(500).send({message: err.message});
+        const num = 0;
+
+        if (!err) res.status(200).render('horario', { isAuthenticated, horario: docs, num });
+        else res.status(500).send({ message: err.message });
     });
-    
+
 };
 
 exports.listCreateHorario = (req, res) => {
-        const user = req.user;    
-        const isAuthenticated = !!req.user;
-        const num = 0;
-        FrequenciaModel.frequenciaList((fdocs,err) =>{
-            if (err) res.status(500).send({message: err.message});
-            CarreiraModel.carreiraList((cdocs,err) =>{                
-        if(!err) res.status(200).render('horariocreate',{isAuthenticated,frequencia:fdocs,carreira:cdocs,user,num});        
-        else res.status(500).send({message: err.message});
+    const user = req.user;
+    const isAuthenticated = !!req.user;
+    const num = 0;
+    FrequenciaModel.frequenciaList((fdocs, err) => {
+        if (err) res.status(500).send({ message: err.message });
+        CarreiraModel.carreiraList((cdocs, err) => {
+            if (!err) res.status(200).render('horariocreate', { isAuthenticated, frequencia: fdocs, carreira: cdocs, user, num });
+            else res.status(500).send({ message: err.message });
         });
-   
+
     });
 };
 
@@ -74,8 +76,8 @@ exports.listCarreiraPool = (req, res) => {
     HorarioModel.horarioList((docs, err) => {
         const isAuthenticated = !!req.user;
         const num = 0;
-        if(!err) res.status(200).render('horariopool',{isAuthenticated,horario:docs,num});
-        else res.status(500).send({message: err.message});
+        if (!err) res.status(200).render('horariopool', { isAuthenticated, horario: docs, num });
+        else res.status(500).send({ message: err.message });
     });
 };
 
@@ -83,23 +85,23 @@ exports.getById = (req, res) => {
     HorarioModel.horarioFindById(req.params.id, (doc, err) => {
         const isAuthenticated = !!req.user;
         const user = req.user;
-        if(!err) res.status(200).render('horarioedit',{isAuthenticated,horario:doc,user});
-        else res.status(500).send({message: err.message});
+        if (!err) res.status(200).render('horarioedit', { isAuthenticated, horario: doc, user });
+        else res.status(500).send({ message: err.message });
     });
 };
 
 exports.patchById = (req, res) => {
     HorarioModel.patchHorario(req.params.id, req.body, (doc, err) => {
-        if(!err) res.status(200).redirect("/horario/pool");
-        else res.status(500).send({message: err.message});
+        if (!err) res.status(200).redirect("/horario/pool");
+        else res.status(500).send({ message: err.message });
     });
 };
 
 exports.removeById = (req, res) => {
 
     HorarioModel.horarioRemoveById(req.params.id, (err) => {
-        if(!err) res.status(204).redirect("/horario/pool");
-        else res.status(500).send({message: err.message});
+        if (!err) res.status(204).redirect("/horario/pool");
+        else res.status(500).send({ message: err.message });
     });
 
 };
