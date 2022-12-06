be963901c6a3d9f36052baf0bcbd7aaa22a1457f
emanuel <emanueljoao80@gmail.com>
2022-12-06 22:54:32 +0000

    pesquisa falta validar horarios vazios

--- controllers/pesquisaController.js
+++ controllers/pesquisaController.js
@@ -14,7 +14,7 @@ exports.listPesquisaHorarios = (req, res) => {
     HorarioModel.findHorarioCarreriaID(req.body.Carreira,(docs, err) => {
         const isAuthenticated = !!req.user;
         const num = 0;
-        if(!err) res.status(200).render('horariofrequencia',{isAuthenticated,carreira:docs,num});
+        if(!err) res.status(200).render('horariofrequencia',{isAuthenticated,horario:docs,num});
         else res.status(500).send({message: err.message});
     });
 };
