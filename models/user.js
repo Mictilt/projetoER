1af2ccc364c0bd7c51f7ab35cb091aca0834d170
Mictilt <jvmiguelito235@gmail.com>
2022-12-06 20:50:32 +0000

    mudanças user

--- models/user.js
+++ models/user.js
@@ -7,22 +7,15 @@ const Ticket = require("../models/ticket");
 const userSchema = new mongoose.Schema({
     username: String,
     email: String,
+    tipo: String,
     password: String,
-    FAQ:[
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'FAQ'
-        }
-    ],
-    responses: [String],
 });
 
 //Create the actual model
 const User = mongoose.model('Users', userSchema);
 
 exports.userFindById = (id, cb) => {
-    User.findById(id, {  _id:1, username:1, password:1, email:1, FAQ:1, responses:1})
-        .populate({ path: 'FAQ', model: FAQ.faqModel() })
+    User.findById(id, {  _id:1, username:1, password:1, email:1, tipo:1})
         .exec()
         .then(doc => cb(doc))
         .catch(err => cb(null, err));
@@ -40,8 +33,7 @@ exports.createUser = (userData, cb) => {
 
 exports.list = (cb) => {
 
-    User.find({ }, { _id:1, username:1, password:1, email:1, FAQ:1, responses:1})
-        .populate({ path: 'FAQ', model: FAQ.faqModel() })
+    User.find({ }, { _id:1, username:1, password:1, email:1, tipo:1})
         .exec()
         .then((docs) => cb(docs))
         .catch(err => cb(err));
@@ -50,8 +42,7 @@ exports.list = (cb) => {
 exports.patchUser = (id, userData, cb) => {
 
     //status code 204 should be returned if we don't want to send back the updated model
-    User.findOneAndUpdate({_id: id}, userData, {new:true, overwrite:true, projection: {  _id:0, username:0, password:0, email:0, FAQ:0, responses:1}})
-        .populate({ path: 'FAQ', model: FAQ.faqModel() })
+    User.findOneAndUpdate({_id: id}, userData, {new:true, overwrite:true, projection: {  _id:0, username:0, password:0, email:0, tipo:1}})
         .exec()
         .then(() => cb())
         .catch(err => cb(err));
